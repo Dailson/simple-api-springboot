5ce7c3d587f2e6cef64ad74be9502c2bec6c29d3
Dailson Gomes <daylsongomes@gmail.com>
2021-06-26 12:47:49 -0300

    Video 14: H2 DATABASE

--- src/main/java/com/dailson/api/domain/Anime.java
+++ src/main/java/com/dailson/api/domain/Anime.java
@@ -2,16 +2,26 @@ package com.dailson.api.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Anime implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
 	
+	public Anime() {
+	}
 
 	public Anime(Long id, String name) {
-		this.id = id;
+		this.id = id; 
 		this.name = name;
 	}
 
