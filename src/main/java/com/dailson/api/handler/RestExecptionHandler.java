994b9de40d7190b2fa685d7bd648e39b644ac0d1
Dailson Gomes <daylsongomes@gmail.com>
2021-07-02 20:22:08 -0300

    Video 23: SPRING HANDLER OVERRIDE

--- src/main/java/com/dailson/api/handler/RestExecptionHandler.java
+++ src/main/java/com/dailson/api/handler/RestExecptionHandler.java
@@ -4,20 +4,25 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.dailson.api.exceptions.BadRequestException;
 import com.dailson.api.exceptions.BadRequestExceptionDetails;
+import com.dailson.api.exceptions.ExceptionDetails;
 import com.dailson.api.exceptions.ValidExceptionDetails;
 
 @ControllerAdvice
 
-public class RestExecptionHandler {
+public class RestExecptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(BadRequestException.class)
 	public ResponseEntity<BadRequestExceptionDetails> handleBadRequestExceptions(BadRequestException exception){
@@ -30,9 +35,9 @@ public class RestExecptionHandler {
 					LocalDateTime.now()), HttpStatus.BAD_REQUEST);					
 	}
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidExceptionDetails> handerValidException(
-			MethodArgumentNotValidException exception){
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request){
 		
 		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 
@@ -49,6 +54,20 @@ public class RestExecptionHandler {
 					fields,
 					fieldsMessage),HttpStatus.BAD_REQUEST);
 		
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(
+			Exception exception, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+				var exceptionDetails = new ExceptionDetails(
+					exception.getCause().getMessage(),
+					status.value(),
+					exception.getMessage(),
+					exception.getClass().getName(),
+					LocalDateTime.now());		
+		
+		return new ResponseEntity<>(exceptionDetails, headers, status);
 	}
 
 }
