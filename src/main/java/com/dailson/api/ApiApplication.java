e58b54a8f42852ed3fb9ae7bf9cb60d2ea6de492
Dailson Gomes <daylsongomes@gmail.com>
2021-07-14 14:55:24 -0300

    Video 51: GRAFANA

--- src/main/java/com/dailson/api/ApiApplication.java
+++ src/main/java/com/dailson/api/ApiApplication.java
@@ -1,7 +1,12 @@
 package com.dailson.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +15,9 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+	    @Value("${spring.application.name}") String applicationName) {
+	    return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
